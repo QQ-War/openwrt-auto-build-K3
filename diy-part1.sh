6cef19a31b7c49e723e4f8de1550bf13dec337f4
QQ-War <2991889+QQ-War@users.noreply.github.com>
2023-02-16 10:50:01 -0300

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,9 +17,9 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git helloworld https://github.com/fw876/helloworld  >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall  >>feeds.conf.default
-echo 'src-git wrtbwmon https://github.com/brvphoenix/wrtbwmon  >>feeds.conf.default
-echo 'src-git luci_wrtb https://github.com/brvphoenix/luci-app-wrtbwmon  >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages  >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small  >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld'  >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall'  >>feeds.conf.default
+echo 'src-git wrtbwmon https://github.com/brvphoenix/wrtbwmon'  >>feeds.conf.default
+echo 'src-git luci_wrtb https://github.com/brvphoenix/luci-app-wrtbwmon'  >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'  >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small'  >>feeds.conf.default
